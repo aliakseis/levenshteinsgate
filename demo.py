fc409a31194d91b234b75cb27ae6650dabf9c0ee
aliakseis <aliaksei.sanko@gmail.com>
2022-09-16 14:35:18 +0300

    timing

--- demo.py
+++ demo.py
@@ -1,13 +1,19 @@
 from levenshteinsgate import Trie
 from pathlib import Path
+import time
 
 twl06 = Path('twl06.txt').read_text().split()
 trie = Trie(twl06)
 
 david_22719 = Path('david_22719.in').read_text().split()
 
+print('Starting...')
+start_time = time.time()
+
 sum = 0
 for v in david_22719:
     sum += trie.min_distance(v)
 
 print(sum)
+
+print("%s seconds" % (time.time() - start_time))
